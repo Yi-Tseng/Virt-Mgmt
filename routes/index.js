2aeecab003959f09e77128c3bc55357fea36adcd
Yi Tseng <a86487817@gmail.com>
2016-11-11 00:16:35 +0800

    add basic onformation page

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+let express = require('express')
+let router = express.Router()
+let Domain = require('../models/domain').Domain
+let Host = require('../models/host').Host
+
+let domainModel = new Domain()
+let hostModel = new Host()
+
+router.get('/', function(req, res, next) {
+    res.render('index', { domains: domainModel.list(), hostname: hostModel.getHostname() })
+});
+
+module.exports = router;
