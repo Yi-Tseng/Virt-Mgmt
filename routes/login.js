d47b7b14633b7c0fbdd0c7787dffc59ddc7c0d30
Yi Tseng <a86487817@gmail.com>
2016-11-11 13:18:45 +0800

    fix error

--- routes/login.js
+++ routes/login.js
@@ -3,11 +3,11 @@ let router = express.Router()
 let User = require('../models/user').User
 let userModel = new User()
 
-router.get('/', async (req, res, next) {
+router.get('/', async (req, res, next) => {
   res.render('login')
 })
 
-router.post('/', async (req, res, next) {
+router.post('/', async (req, res, next) => {
   let result = await userModel.auth(req.body.username, req.body.password)
   if (result) {
     req.session.username = req.body.username
